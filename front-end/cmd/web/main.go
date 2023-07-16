90bc6e6cad0223be961f8543bfeb8ad3dbe303e5
anvov <andriy.v@overdose.digital>
2023-07-16 16:23:36 +0300

    front-end

---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "start.game.gohtml")
+	})
+
+	fmt.Println("Starting front end service on port 80")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	partials := []string{
+		fmt.Sprintf("./cmd/web/templates/%s", t),
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/header.partial.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+
+	tmpl, err := template.ParseFiles(partials...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
